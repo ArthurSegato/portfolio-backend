f8b5e06f89351f19d6d3e42a2c29328f34a28ba0
unknown <github.reentry594@passinbox.com>
2023-08-26 03:29:03 -0300

    Add discord WebHook

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ContactDto } from './dto/contact.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ContactService {
+  constructor(private config: ConfigService) { }
+
   async send(dto: ContactDto) {
     try {
-      // Manda mensagem pro bot do discord
+      // Omite o numero de telefone do webhook
+      if (dto.phone === undefined || dto.phone === null) dto.phone = ''
+
+      const url = this.config.get('WEBHOOK_URL');
+
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "content": `${dto.name} ${dto.email} ${dto.phone} ${dto.message}`
+        })
+      })
+
       return { message: 'Your message has been received' };
     }
     catch (error) {
